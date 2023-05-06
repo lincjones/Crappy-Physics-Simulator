7ed1f9b894988eb9133e1a30d7ccf4fb354453c2
Boolean <81984518+lincjones@users.noreply.github.com>
2023-05-06 14:26:30 -0400

    Delete CrappyPlatformer directory

--- CrappyPlatformer/src/include/settings.h
+++
@@ -1,2 +0,0 @@
-const int window_height = 600;
-const int window_width = 900;
--- CrappyPlatformer/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <raylib.h>
-#include "include/Game.hpp"
-
-int main() {
-    Game game;
-    while(!WindowShouldClose()) {
-        game.Run();
-    }
-}
